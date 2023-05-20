babb9f2d0079f458fe2eadab9e48c44066fcb18f
Carlos Doffiny S-V <carlosdoffinysv@gmail.com>
2023-05-20 11:53:27 -0400

    merge: 🔀 Merge features/Parser in features/Operators

---
+++ src/commands/operators/IsNull.ts
@@ -0,0 +1,24 @@
+import IOperator from './IOperator';
+import ICommand, { isCommand } from '../ICommand';
+import { registerOperator } from '../index';
+
+@registerOperator('isNull')
+export default class IsNull implements IOperator<boolean> {
+  id = 'isNull';
+  symbol = 'isNull';
+
+  operators: (ICommand<unknown> | unknown)[];
+  constructor(...operators: (ICommand<unknown> | unknown)[]) {
+    this.operators = operators;
+  }
+
+  execute(): boolean {
+    return this.operators.some((operator) => {
+      if (isCommand(operator)) {
+        const value = operator.execute();
+        return value === null || value === undefined;
+      }
+      return operator === null || operator === undefined;
+    });
+  }
+}
